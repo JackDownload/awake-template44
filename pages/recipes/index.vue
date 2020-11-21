c60a40a94776c507ebeff43c643e25155e715bf6
Mocsonoky Attila <kismocsy@gmail.com>
2020-11-21 10:54:40 +0100

    Update index.vue

--- pages/recipes/index.vue
+++ pages/recipes/index.vue
@@ -1,11 +1,27 @@
 <template>
-  <div id="recipes-page" class="page-wrapper recipes-page">
-    <site-hero title="Recipes" image="https://picsum.photos/1800/1801" />
+  <div id="home-page" class="page-wrapper home-page">
+    <site-hero :title="title" :subtitle="subtitle" :image="featureImage">
+      <button
+        v-if="$siteConfig.newsletter.on"
+        class="button is-primary"
+        @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
+      >
+        Subscribe To Newsletter
+      </button>
+    </site-hero>
     <main-section theme="one-column">
-      <recipes-grid />
+      <template v-slot:default>
+        <!-- All Recipess -->
+        <recipes-grid />
+      </template>
+      <template v-slot:sidebar>
+        Nothing here
+      </template>
     </main-section>
+    <news-letter-form-modal />
   </div>
 </template>
+
 <script>
 export default {
   head() {
