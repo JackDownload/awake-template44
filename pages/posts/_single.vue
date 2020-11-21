1af2ff5969997ba00b8bd440afbd3a69120ce23e
Mocsonoky Attila <kismocsy@gmail.com>
2020-11-21 13:32:37 +0100

    Update _single.vue

--- pages/posts/_single.vue
+++ pages/posts/_single.vue
@@ -56,7 +56,7 @@ export default {
       return getFormattedDate(this.$store.state.date)
     },
     url() {
-      return `${process.env.URL}/${this.$route.fullPath}/posts/`
+      return `${process.env.URL}/${this.$route.fullPath}`
     }
   },
   fetch({ store, params }) {
