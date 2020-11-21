a99a6f0da7970cfb2d8cd55a53c90c929141d92d
JackDownload <kismocsy@gmail.com>
2020-11-21 09:14:10 +0100

    Update generate-recipe-api.js

--- cms/netlify/build/generate-recipe-api.js
+++ cms/netlify/build/generate-recipe-api.js
@@ -1,1 +1,22 @@
+import siteConfig from '../../../config/_siteConfig'
+import { createPagination, createMeta, createAll } from './helper'
+const rootDir = `${__dirname}/../../..`
+const generateRecipeApi = () => {
+  const contentDir = `${rootDir}/content/recipes`
+  const apiDir = `${rootDir}/static/api`
+  const allFile = `${apiDir}/recipes.json`
+  const metaFile = `${apiDir}/recipes-meta.json`
+  const pages = siteConfig.posts.perPage
+  const doneMessage = `{totalNumber} recipes generated in posts API`
 
+  createAll(contentDir, allFile, apiDir)
+    .then((all) => {
+      const totalPages = createPagination(pages, all, `${apiDir}/recipes`)
+      createMeta({ totalPages: totalPages }, metaFile)
+      console.log(doneMessage.replace('{totalNumber}', all.length))
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
+export default generateRecipeApi
